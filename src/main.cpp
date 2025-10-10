ebefd0db2774561b03a3e09657cfca2319ba4e77
lyro7 <lovro.lupis7@gmail.com>
2025-10-10 06:22:59 +0200

    Add project files.

---
+++ src/main.cpp
@@ -0,0 +1,42 @@
+﻿#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+static void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+	glViewport(0, 0, width, height);
+}
+
+int main() {
+	if (!glfwInit()) {
+		std::cerr << "Error while initializing GLFW" << std::endl;
+		return -1;
+	}
+	
+	GLFWwindow* window = glfwCreateWindow(640, 480, "Raytracer", nullptr, nullptr);
+	if (window == nullptr) {
+		std::cerr << "Failed to create window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+	glfwSwapInterval(1);
+
+	if (!gladLoadGLLoader((GLADloadproc) glfwGetProcAddress)) {
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		return -1;
+	}
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+	while (!glfwWindowShouldClose(window)) {
+		glClearColor(0.2f, 0.2f, 0.2f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		// Do work
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+	glfwTerminate();
+
+	return 0;
+}
