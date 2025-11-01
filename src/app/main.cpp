02ee8c25c48897b6117e4b40d9a7c7e31514d8f0
lyro7 <lovro.lupis7@gmail.com>
2025-11-01 17:37:57 +0100

    feat(main): Initialized the compute program

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,17 +1,27 @@
-#include "loader.h"
+#include <glad/glad.h>
+#include "object_loader.h"
+#include "compute_program.h"
 #include "core/window.h"
 #include <iostream>
 
 int main() {
-    Window window(640, 480, "Raytracer");
+    Window window(400, 400, "Raytracer");
 
-    Mesh mesh = Loader::loadMesh("assets/models/sphere.obj");
+    Mesh mesh = ObjectLoader::loadMesh("assets/models/sphere.obj");
 
+    const GLsizei raytraceResX = 3000;
+    const GLsizei raytraceResY = 3000;
+
+    ComputeProgram computeProgram(raytraceResX, raytraceResY, mesh);
+    GLuint computeShader = computeProgram.createComputeShader("shaders/compute.glsl");
+    GLuint program = computeProgram.createComputeProgram(computeShader);
+    computeProgram.startComputeProgram(program);
+    
     while (!window.shouldClose()) {
         glClearColor(0.2f, 0.2f, 0.2f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        // Do work
+        computeProgram.dispatchCompute();
 
         window.swapBuffers();
         window.pollEvents();
