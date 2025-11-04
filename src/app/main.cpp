42f649215c1bf5503f07f9ce3d15a47dabb4ab68
lyro7 <lovro.lupis7@gmail.com>
2025-11-04 16:08:01 +0100

    feat(main): Initialized compute & graphics pipeline.

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,6 +1,7 @@
 #include <glad/glad.h>
 #include "object_loader.h"
 #include "compute_program.h"
+#include "render_program.h"
 #include "core/window.h"
 #include <iostream>
 
@@ -12,16 +13,25 @@ int main() {
     const GLsizei raytraceResX = 3000;
     const GLsizei raytraceResY = 3000;
 
-    ComputeProgram computeProgram(raytraceResX, raytraceResY, mesh);
-    GLuint computeShader = computeProgram.createComputeShader("shaders/compute.glsl");
-    GLuint program = computeProgram.createComputeProgram(computeShader);
-    computeProgram.startComputeProgram(program);
-    
-    while (!window.shouldClose()) {
-        glClearColor(0.2f, 0.2f, 0.2f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
+    GLuint outTex = 0;
 
+    // Init compute program
+    ComputeProgram computeProgram(raytraceResX, raytraceResY, mesh, &outTex);
+    GLuint computeShader = computeProgram.createComputeShader("shaders/compute.glsl");
+    GLuint cProgram = computeProgram.createComputeProgram(computeShader);
+    // Init render program
+    RenderProgram renderProgram(raytraceResX, raytraceResY, outTex);
+    GLuint vertexShader = renderProgram.createVertexShader("shaders/vertex.glsl");
+    GLuint fragmentShader = renderProgram.createFragmentShader("shaders/fragment.glsl");
+    GLuint rProgram = renderProgram.createRenderProgram(vertexShader, fragmentShader);
+    renderProgram.initRenderResources(rProgram);
+
+    while (!window.shouldClose()) {
+        glClear(GL_COLOR_BUFFER_BIT);
+        computeProgram.startComputeProgram(cProgram);
         computeProgram.dispatchCompute();
+        renderProgram.startRenderProgram(rProgram);
+        renderProgram.render();
 
         window.swapBuffers();
         window.pollEvents();
