a645f23fc650fb282df85473aeb27cc040c55abd
lyro7 <lovro.lupis7@gmail.com>
2025-11-18 04:23:20 +0100

    feat(main): Wired scene into new engine and UI setup

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,48 +1,35 @@
+#include <glad/glad.h>
+#include <iostream>
 #include "compute_program.h"
 #include "render_program.h"
 #include "core/window.h"
 #include "object_loader.h"
-#include <glad/glad.h>
-#include <iostream>
-// Control ImGui and connect it to GLFW/OpenGL.
 #include "imgui.h"
 #include "imgui_impl_glfw.h"
 #include "imgui_impl_opengl3.h"
 #include "../ui/raytracer_ui.h"
 
-
 int main()
 {
 	Window window(1280, 720, "Raytracer");
 
-	Mesh mesh = ObjectLoader::loadMesh("assets/models/sphere.obj");
+    Scene scene;
 
-	const GLsizei raytraceResX = 3000;
-	const GLsizei raytraceResY = 3000;
+    // Base raytrace resolution
+	const GLsizei width = 1280;
+	const GLsizei height = 720;
 
-	GLuint outTex = 0;
-
-	// Init compute program
-	ComputeProgram computeProgram(raytraceResX, raytraceResY, mesh, &outTex);
-	GLuint computeShader = computeProgram.createComputeShader("shaders/compute.glsl");
-	GLuint cProgram = computeProgram.createComputeProgram(computeShader);
-	// Init render program
-	RenderProgram renderProgram(raytraceResX, raytraceResY, outTex);
-	GLuint vertexShader = renderProgram.createVertexShader("shaders/vertex.glsl");
-	GLuint fragmentShader = renderProgram.createFragmentShader("shaders/fragment.glsl");
-	GLuint rProgram = renderProgram.createRenderProgram(vertexShader, fragmentShader);
-
-	renderProgram.initRenderResources(rProgram);
+    RaytracerEngine engine(width, height, scene);
 
 	// UI
-    RaytracerUI ui;
+    RaytracerUI ui(engine, scene);
     ui.init(window);
 
     while (!window.shouldClose()) 
 	{
         window.pollEvents();
         ui.beginFrame();
-        ui.draw(computeProgram, renderProgram, cProgram, rProgram);
+        ui.draw();
         ui.endFrame();
     }
 
