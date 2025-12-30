863583c2034a552ca8b66434755e5166d33b5cbf
altonbekolli <altonbekolli_1@web.de>
2025-12-30 22:56:23 +0100

    chore(external):add miniz for zip support

---
+++ external/miniz/miniz_export.h
@@ -0,0 +1,7 @@
+#pragma once
+
+// Minimal export macro for single-translation-unit builds.
+// We don't build miniz as a DLL in this project, so empty is fine.
+#ifndef MINIZ_EXPORT
+#define MINIZ_EXPORT
+#endif
