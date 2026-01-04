89c69a062e608e7b99eb7710ffefed4fb61fe830
altonbekolli <altonbekolli_1@web.de>
2026-01-04 16:57:57 +0100

    src(ui):add func to reset json

--- src/ui/raytracer_ui.cpp
+++ src/ui/raytracer_ui.cpp
@@ -117,7 +117,7 @@ void RaytracerUI::drawTool()
 			{
 				if (ImGui::MenuItem("Reset Environment"))
 				{
-					// TODO: reset to default
+					resetEnvironment();
 				}
 
 				if (ImGui::MenuItem("Exit"))
@@ -611,4 +611,29 @@ void RaytracerUI::syncActiveSceneJsonFromScene()
 
 	// Update the string shown in UI + used for export
 	m_activeSceneJson = m_activeSceneJsonObj.dump(2);
+}
+
+void RaytracerUI::resetEnvironment()
+{
+	// mesh & camera & light reset
+	scene.reset();
+
+	// 2) UI state reset
+	bg[0] = 0.0f;
+	bg[1] = 0.0f;
+	bg[2] = 0.0f;
+
+	showRaytraced = false;
+
+	// 3) Active JSON reset
+	m_activeSceneJson.clear();
+	m_activeSceneJsonObj = nlohmann::json{};
+
+	// 4) Zip-Request cleanup
+	m_requestLoadZip = false;
+	m_requestedZipPath.clear();
+
+	engine.onSceneChanged();
+	engine.clearOutputTextures(0, 0, 0, 1);
+	raytraceRequested = false;
 }
